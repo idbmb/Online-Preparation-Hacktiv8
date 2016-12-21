175a4b13184c48235761e824ef947bc555a251e5
idbmb <bmb.router@gmail.com>
2016-12-21 10:40:38 +0700

    added pemanggilan parameter

--- Phase-0/Week-2/Kamis/object2.js
+++ Phase-0/Week-2/Kamis/object2.js
@@ -1,28 +1,27 @@
 var Teman = function (nama, kabar, pekerjaan) {
-      this.nama = nama;
-      this.kabar = kabar;
-      this.pekerjaan = pekerjaan;
-      this.sapa = function () {
-console.log('Hallo '+ this.nama + ', apa kabar ?');
-      };
-      this.balasSapa = function () {
-console.log('Hallo ' + this.nama + ', kabarku ' + this.kabar);
-      };
-      this.tanyaPekerjaan = function () {
-console.log('Apa pekerjaanmu saat ini?');
-      };
-      this.balasPekerjaan = function () {
-console.log('Pekerjaanku saat ini adalah ' + this.pekerjaan);
-      };
-      this.berpisah = function (teman) {
-console.log('Senang bertemu denganmu '+ teman.nama +' , semoga kamu sukses sebagai '+ teman.pekerjaan);
-      };
-    }
-
+  this.nama = nama;
+  this.kabar = kabar;
+  this.pekerjaan = pekerjaan;
+  this.sapa = function (nama) {
+    console.log('Hallo '+ nama + ', apa kabar ?');
+  };
+  this.balasSapa = function (nama) {
+    console.log('Hallo ' + nama + ', kabarku ' + this.kabar);
+  };
+  this.tanyaPekerjaan = function () {
+    console.log('Apa pekerjaanmu saat ini?');
+  };
+  this.balasPekerjaan = function () {
+    console.log('Pekerjaanku saat ini adalah ' + this.pekerjaan);
+  };
+  this.berpisah = function (teman) {
+    console.log('Senang bertemu denganmu '+ teman.nama +' , semoga kamu sukses sebagai '+ teman.pekerjaan);
+  };
+}
 var budi = new Teman('Budi', 'baik', 'developer');
 var tono = new Teman('Tono', 'baik', 'chef');
-    tono.sapa();
-    budi.balasSapa();
-    budi.tanyaPekerjaan();
-    tono.balasPekerjaan();
-    budi.berpisah(tono);
+budi.sapa(tono.nama);
+tono.balasSapa(budi.nama);
+budi.tanyaPekerjaan();
+tono.balasPekerjaan();
+budi.berpisah(tono);
