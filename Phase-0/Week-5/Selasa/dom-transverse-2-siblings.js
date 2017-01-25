140d8594a99d9c7203c8890f8c4c7d957b7d35f7
idbmb <bmb.router@gmail.com>
2017-01-25 09:54:00 +0700

    Learn Transversing in DOM

---
+++ Phase-0/Week-5/Selasa/dom-transverse-2-siblings.js
@@ -0,0 +1,22 @@
+/*
+=============================================
+ Menseleksi DOM berdasarkan hubungan Sibling
+=============================================
+*/
+
+// Part 1
+
+// Menseleksi element <div id="contoh-div-1">
+var contohDiv1 = document.getElementById('contoh-div-1');
+
+// Mendapatkan sibling setelah <div id="contoh-div-1">
+var contohDiv1NextSibling = contohDiv1.nextElementSibling;
+
+console.log(contohDiv1NextSibling); // <div id="contoh-div-2">...</div>
+
+// Part 2
+
+// Mendapatkan sibling sebelum <div id="contoh-div-1">
+var contohDiv1PrevSibling = contohDiv1.previousElementSibling;
+
+console.log(contohDiv1PrevSibling); // <h1></h1>
